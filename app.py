16dc60f6e7b77e1cb800546b505076049ad2076b
mhsuhail00 <mhsuhail00@gmail.com>
2025-06-10 11:13:14 +0530

    Initial Commit v2 includes partial launchScreen

---
+++ app.py
@@ -0,0 +1,32 @@
+from src.ui.windows.launchScreen_UI import OsdagLaunchScreen
+
+from PySide6.QtWidgets import QMainWindow, QApplication
+from PySide6.QtCore import QThread, Signal
+
+class LoadingThread(QThread):
+    finished = Signal()
+
+    def run(self):
+        # Simulate loading (replace with your real setup)
+        import time
+        time.sleep(15)
+        self.finished.emit()
+
+class LaunchScreenPopup(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = OsdagLaunchScreen()
+        self.ui.setupUi(self)
+        self.show()
+
+        self.loader = LoadingThread()
+        self.loader.finished.connect(self.close_and_launch)
+        self.loader.start()
+
+    def close_and_launch(self):
+        self.close()
+
+if __name__ == '__main__':
+    app = QApplication([])
+    popup = LaunchScreenPopup()
+    app.exec()
